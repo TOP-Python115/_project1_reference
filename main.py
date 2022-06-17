4a0b9bf0ccd0fcc5c48f517df8be8533f592550e
Gennadiy <renardes@gmail.com>
2022-06-17 09:33:18 +0500

    добавление взаимодействия с таблицей результатов

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Модуль верхнего уровня приложения Крестики-Нолики
 # Copyright by Gennadiy S. aka GennDALF
 
-from players import *
+from players import read_ini, player_name, game_mode, modify_stats, show_stats, PLAYER
 from help import show_help, show_message, COMMANDS, MESSAGES
+from field import game
 
 
 # приветствие
@@ -25,18 +26,20 @@ while True:
         show_help()
     # показать таблицу результатов
     elif command in COMMANDS['scores']:
-        pass
+        show_stats(table=True)
     # начало новой партии
     elif command in COMMANDS['new']:
         # есть ли текущий игрок
         if not PLAYER:
             # запрос имени игрока
             player_name()
-        #
+        # запрос режимов игры
         if game_mode():
             # продолжаем сохранённую партию
-            pass
+            modify_stats(game(load=True))
         else:
             # начинаем новую партию
-            pass
-
+            modify_stats(game())
+        # после завершения партии показываем статистику
+        #   для участвовавших игроков людей
+        show_stats()
