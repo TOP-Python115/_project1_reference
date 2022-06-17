954bf6d710da114ce858efc5f05b2f5a8e00596e
Gennadiy <renardes@gmail.com>
2022-06-17 09:27:05 +0500

    правка раздела помощи
    Добавление текста раздела помощи
    Модификация функции вывода помощи

--- help.py
+++ help.py
@@ -2,15 +2,16 @@
 # Copyright by Gennadiy S. aka GennDALF
 
 from shutil import get_terminal_size as gts
-from math import floor, ceil
+from math import ceil, floor
 
-COMMANDS = {'quit': ('quit', 'выход'),
+import field
+
+COMMANDS = {'quit': ('quit', 'выход', 'q'),
             'help': ('help', 'помощь', 'h', '?'),
             'scores': ('scores', 'таблица'),
-            'new': ('new', 'новая', 'yes', 'да', 'y', 'д'),
+            'new': ('new', 'новая', 'yes', 'да', 'y', 'д', '1'),
             '': (),
             }
-
 MESSAGES = ('хотите начать новую партию? > ',
             'введите имя игрока > ',
             'введите имя второго игрока > ',
@@ -19,26 +20,27 @@ MESSAGES = ('хотите начать новую партию? > ',
             'вы хотите ходить первым? > ',
             'вы хотите загрузить сохранённую партию? > ',
             )
-
 ANSWERS = (None,
            None,
            None,
            ('1', 'бот', 'б', '2', 'человек', 'ч'),
            ('1', 'лёгкий', 'л', '2', 'трудный', 'т'),
-           ('yes', 'да', 'y', 'д'),
-           ('yes', 'да', 'y', 'д'),
+           ('yes', 'да', 'y', 'д', '1'),
+           ('yes', 'да', 'y', 'д', '1'),
            )
 
-
-h = f"""Правила игры:
-
-Список команд:
-{' '.join(COMMANDS['quit'])}
-"""
+h_r = f"""Правила игры:
+    Вы играете одним из двух символов: крестиком '{field.SYMBOLS[0]}' или ноликом '{field.SYMBOLS[1]}'. Первым составьте последовательность из {field.DIM} символов в одной строке, в одном столбце, либо в одной диагонали."""
+h_c1 = f"""Список команд:
+Между партиями вы можете использовать следующие команды:"""
 
 
 def show_help():
-    print(h)
+    print(h_r, end='\n\n')
+    field.show_field([[f'{i},{j}' for j in range(field.DIM)]
+                      for i in range(field.DIM)], center=True)
+    print(h_c1, end='\n\n')
+    print('\n'.join(['  ' + ' '.join(COMMANDS[comm]) for comm in COMMANDS]))
 
 def show_message(text=''):
     width = gts()[0] - 1
